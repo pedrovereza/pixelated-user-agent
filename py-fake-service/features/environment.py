d950aa161c6d95b6fc4a97dec418e6d62854cfbf
Folker Bernitt <fbernitt@thoughtworks.com>
2014-08-20 17:17:18 +0200

    Switched to phantomjs.

--- py-fake-service/features/environment.py
+++ py-fake-service/features/environment.py
@@ -2,7 +2,8 @@ from selenium import webdriver
 
 
 def before_feature(context, feature):
-    context.browser = webdriver.Firefox()
+    #context.browser = webdriver.Firefox()
+    context.browser = webdriver.PhantomJS()
     context.browser.set_window_size(1280, 1024)
     context.browser.implicitly_wait(5)
     context.browser.set_page_load_timeout(60) # wait for data
@@ -12,8 +13,10 @@ def before_feature(context, feature):
 def after_feature(context, feature):
     context.browser.quit()
 
+
 def take_screenshot(context):
     context.browser.save_screenshot('/tmp/screenshot.jpeg')
+
 
 def save_source(context):
     with open('/tmp/source.html', 'w') as out:
